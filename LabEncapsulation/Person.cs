54fc3f02fe856dcede2845fa73a756c0c65d2025
MarekL-MarekL <markandmark3@gmail.com>
2024-03-12 14:03:19 +0100

    Add project files.

---
+++ LabEncapsulation/Person.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabEncapsulation
+{
+    internal class Person
+    {
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set {
+                if (firstName.Length<3) 
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols");
+                }
+            
+            }
+        }   
+
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName.Length<3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+            }
+        }
+
+        private int age;
+
+        public int Age 
+        { 
+        get { return age; }
+            set
+            {
+                if (value <1)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+         }
+
+        private decimal salary;
+        public decimal Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value < 460) 
+                {
+                    throw new ArgumentException("Salary cannot be less then 460 dollar");
+                }
+                else
+                {
+                    salary = value;
+                }
+            }
+        }
+        private decimal IncreasedSalary { get; set; }
+
+
+
+
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+            IncreasedSalary = salary;
+           
+        }
+
+       
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age<30)
+            {
+                IncreasedSalary = Salary + (Salary*percentage/200);
+
+            }
+            else
+            {
+                IncreasedSalary = Salary + (Salary * percentage / 100);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old recive salary {Salary}  and after increase salary is {IncreasedSalary}";
+        }
+
+    }
+
+
+}
---
+++ LabEncapsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace LabEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Anna", "Persson", 25,2000);
+            Person person2 = new Person("Magnus", "Petterson", 65,3000);
+            Person person3 = new Person("Johanna", "Eriksson", 57,4000);
+            Person person4 = new Person("Peter", "Forsberg", 27,5000);
+            Person person5 = new Person("Erika", "Samuelsson", 35,6000);
+
+            List<Person> personList = new List<Person> { person1, person2, person3, person4, person5 };
+
+            foreach (Person person in personList)
+            {
+                person.IncreaseSalary(20);
+                Console.WriteLine(person.ToString());
+            }
+
+        }
+    }
+}
